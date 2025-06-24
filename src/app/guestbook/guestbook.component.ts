56d318d6d899da3dab1bcb83930c1a4e0016337a
xsantcastx <43703069+xsantcastx@users.noreply.github.com>
2025-06-24 16:33:42 +0200

    updatated resposive, and creating feature for post

---
+++ src/app/guestbook/guestbook.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guestbook',
+  templateUrl: './guestbook.component.html',
+  styleUrls: ['./guestbook.component.css'],
+})
+export class GuestbookComponent {
+  nickname = '';
+  message = '';
+  guestbookEntries: { nickname: string; message: string }[] = [];
+
+  submitMessage() {
+    if (this.nickname && this.message) {
+      this.guestbookEntries.unshift({ nickname: this.nickname, message: this.message });
+      this.nickname = '';
+      this.message = '';
+    }
+  }
+}
