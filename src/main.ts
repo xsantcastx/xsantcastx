a02e12dde5182a1b5ee25b71e7abe606cbae0ec0
xsantcastx <43703069+xsantcastx@users.noreply.github.com>
2025-06-24 20:37:47 +0200

    production ready

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,7 @@ import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
 import { browserLocalPersistence, setPersistence } from 'firebase/auth';
 
-// Enable debug mode for Firebase App Check (only for local development — remove in production!)
-(self as any).FIREBASE_APPCHECK_DEBUG_TOKEN = true;
 
-// IMPORTANT: Replace this token with the actual debug token printed in the browser console when you run once with debug enabled.
-// You only need to do this once per browser, and you must add this token in Firebase Console > App Check > Debug tokens
-localStorage.setItem('firebase-appcheck-debug-token', '5AC011C1-EB9F-42DA-9F9D-671E498E088A');
 
 const app = initializeApp(environment.firebase);
 
